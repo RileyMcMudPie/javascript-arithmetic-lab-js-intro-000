32adc9aecda0a0d1f48aada9d8cedc373b3b7b74
Riley McMudPie <rydonamusic@gmail.com>
2020-07-05 03:26:38 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(){}
+
+function add(a,b){
+  return a + b
+}
+
+function subtract(a,b){
+  return a - b
+}
+
+function multiply(a,b){
+  return a * b
+}
+
+function divide(a,b){
+  return a / b
+}
+
+function increment(n){
+  return ++n
+}
+
+function decrement(n){
+  return --n
+}
+
+function makeInt(n){
+  return parseInt('2', 10)
+}
+
+function makeInt(n){
+  return parseInt('2.22', 10)
+  
+}
+function makeInt(n){
+  return parseInt('noopers', 10)
+}
